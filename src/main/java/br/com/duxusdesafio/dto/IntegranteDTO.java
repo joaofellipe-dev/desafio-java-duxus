e41e0536dea078c536991a6b66c1e618df7ae267
joaofellipe-dev <joaofellipesouza90@gmail.com>
2026-09-05 15:30:59 -0300

    feat: criacao pasta dto e classe IntegranteDTO.java

---
+++ src/main/java/br/com/duxusdesafio/dto/IntegranteDTO.java
@@ -0,0 +1,14 @@
+package br.com.duxusdesafio.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class IntegranteDTO {
+    private String nome;
+    private String funcao;
+
+    public IntegranteDTO(){
+    }
+}
