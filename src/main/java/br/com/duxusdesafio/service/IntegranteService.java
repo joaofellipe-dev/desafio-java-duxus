1c10205f38e3877e81f3d00c1a3552026fe02df2
joaofellipe-dev <joaofellipesouza90@gmail.com>
2026-09-06 13:01:58 -0300

    docs(service): documentar regras de negocio e formatar IntegranteService

--- src/main/java/br/com/duxusdesafio/service/IntegranteService.java
+++ src/main/java/br/com/duxusdesafio/service/IntegranteService.java
@@ -4,8 +4,12 @@ import br.com.duxusdesafio.dto.IntegranteDTO;
 import br.com.duxusdesafio.model.Integrante;
 import br.com.duxusdesafio.repository.IntegranteRepository;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+/**
+ * Regras de negócio e operações de persistência para Integrantes.
+ */
 @Service
 public class IntegranteService {
     private final IntegranteRepository integranteRepository;
@@ -13,6 +17,10 @@ public class IntegranteService {
     public IntegranteService(IntegranteRepository integranteRepository) {
         this.integranteRepository = integranteRepository;
     }
+
+    /**
+     * Mapeia os dados recebidos do DTO para a entidade e persiste no banco.
+     */
     public Integrante cadastrar(IntegranteDTO dto) {
         Integrante integrante = new Integrante();
         integrante.setNome(dto.getNome());
@@ -20,7 +28,8 @@ public class IntegranteService {
 
         return integranteRepository.save(integrante);
     }
-    public List<Integrante> listarTodos(){
+
+    public List<Integrante> listarTodos() {
         return integranteRepository.findAll();
     }
 }
