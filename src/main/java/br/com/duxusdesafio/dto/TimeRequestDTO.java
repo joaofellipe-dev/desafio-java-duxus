f4c2b4c90f8d09cf2d80f7f3481ac580b779248f
joaofellipe-dev <joaofellipesouza90@gmail.com>
2026-09-06 12:40:33 -0300

    docs(dto): documentar e formatar TimeRequestDTO

--- src/main/java/br/com/duxusdesafio/dto/TimeRequestDTO.java
+++ src/main/java/br/com/duxusdesafio/dto/TimeRequestDTO.java
@@ -5,12 +5,15 @@ import lombok.Setter;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * DTO de entrada para criação de times via identificadores existentes.
+ */
 @Getter
 @Setter
 public class TimeRequestDTO {
     private String nomeDoClube;
     private LocalDate data;
 
-    private List<Long>integranteIds;
+    private List<Long> integranteIds;
 
 }
