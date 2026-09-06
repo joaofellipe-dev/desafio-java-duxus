ec3fd113761b06c42b0133673a0b0c5354a1491b
joaofellipe-dev <joaofellipesouza90@gmail.com>
2026-09-06 12:37:56 -0300

    docs(dto): documentar e formatar IntegranteDTO

--- src/main/java/br/com/duxusdesafio/dto/IntegranteDTO.java
+++ src/main/java/br/com/duxusdesafio/dto/IntegranteDTO.java
@@ -3,12 +3,16 @@ package br.com.duxusdesafio.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * DTO para recebimento e transferência de dados de Integrante.
+ * Uso explícito de @Getter e @Setter para manter o controle fino dos métodos de acesso.
+ */
 @Getter
 @Setter
 public class IntegranteDTO {
     private String nome;
     private String funcao;
 
-    public IntegranteDTO(){
+    public IntegranteDTO() {
     }
 }
