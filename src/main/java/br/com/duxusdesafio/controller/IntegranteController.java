35f5b578a4d4f6c9d6cfdb4f4be46f77a541a7f9
joaofellipe-dev <joaofellipesouza90@gmail.com>
2026-09-06 13:04:33 -0300

    docs(controller): documentar endpoints e status http em IntegranteController

--- src/main/java/br/com/duxusdesafio/controller/IntegranteController.java
+++ src/main/java/br/com/duxusdesafio/controller/IntegranteController.java
@@ -6,25 +6,37 @@ import br.com.duxusdesafio.service.IntegranteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
+/**
+ * Endpoints REST responsáveis pelo ciclo de vida dos integrantes.
+ */
 @RestController
 @RequestMapping("/integrantes")
 public class IntegranteController {
 
     private final IntegranteService integranteService;
 
-    public IntegranteController(IntegranteService integranteService){
+    public IntegranteController(IntegranteService integranteService) {
         this.integranteService = integranteService;
     }
+
+    /**
+     * Cadastra um novo atleta.
+     * Retorna 201 Created para indicar a criação bem-sucedida do recurso no servidor.
+     */
     @PostMapping
     public ResponseEntity<Integrante> cadastrar(@RequestBody IntegranteDTO dto) {
         Integrante salvo = integranteService.cadastrar(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(salvo);
     }
 
+    /**
+     * Recupera todos os integrantes cadastrados na base.
+     */
     @GetMapping
-    public ResponseEntity <List<Integrante>> listarTodos() {
+    public ResponseEntity<List<Integrante>> listarTodos() {
         return ResponseEntity.ok(integranteService.listarTodos());
     }
 }
