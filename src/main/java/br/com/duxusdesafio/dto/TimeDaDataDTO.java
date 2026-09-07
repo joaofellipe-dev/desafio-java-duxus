852bae90ad3b51845ae00a4cca4ec2a6479d6f73
joaofellipe-dev <joaofellipesouza90@gmail.com>
2026-09-07 18:59:25 -0300

    style: indenta o codigo e  acrescenta comentarios

--- src/main/java/br/com/duxusdesafio/dto/TimeDaDataDTO.java
+++ src/main/java/br/com/duxusdesafio/dto/TimeDaDataDTO.java
@@ -6,6 +6,11 @@ import lombok.Setter;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * Esta classe foi criada especificamente para o endpoint /time-da-data.
+ * Isola a entidade Time e formata a saída JSON para conter apenas a lista de nomes
+ * dos integrantes em formato de String.
+ */
 @Getter
 @Setter
 public class TimeDaDataDTO {
@@ -13,11 +18,12 @@ public class TimeDaDataDTO {
     private String clube;
     private List<String> integrantes;
 
-    public TimeDaDataDTO(){
+    public TimeDaDataDTO() {
     }
-    public TimeDaDataDTO(LocalDate data, String clube, List<String> integrantes){
-       this.data = data;
-       this.clube=clube;
-       this.integrantes = integrantes;
+
+    public TimeDaDataDTO(LocalDate data, String clube, List<String> integrantes) {
+        this.data = data;
+        this.clube = clube;
+        this.integrantes = integrantes;
     }
 }
